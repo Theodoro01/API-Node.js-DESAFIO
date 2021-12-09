6b7bae04816145193ebf4a9a50b88d45d63b3d16
Theodoro01 <theodorovieira01@gmail.com>
2021-12-09 15:43:33 -0300

    API-Node.js--DESAFIO-DADO

--- src/database/database.js
+++ src/database/database.js
@@ -2,20 +2,13 @@
 const mongoose = require("mongoose")
 
 const dotenv = require("dotenv/config")
-
-// const uri = "mongodb+srv://Theodorovieira01:5qbCv0iY7GDNvq5H@cluster0.8oerp.mongodb.net/DesafioDado?retryWrites=true&w=majority"
     
 mongoose.connect(process.env.MONGODB_ATLAS,{ 
         useNewUrlParser: true, useUnifiedTopology: true,});
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
-console.log(process.env.MONGODB_ATLAS)
 
-// mongoose.connect('mongodb://localhost/desafio');
+
+
 mongoose.Promise = global.Promise
 
 module.exports = mongoose;
--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,5 @@
 const  mongoose = require("../database/database")
 
-
-
 const bcrypt = require("bcryptjs")
 const { v4: uuid } = require('uuid');
 
