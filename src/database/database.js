46bac084f6acb81a579873acb4c599db3a820f21
Theodoro01 <theodorovieira01@gmail.com>
2021-12-14 12:29:09 -0300

    API-Node.js--DESAFIO-DADO

--- src/database/database.js
+++ src/database/database.js
@@ -3,8 +3,12 @@ const mongoose = require("mongoose")
 
 const dotenv = require("dotenv/config")
 
+const dbPassword = process.env.DB_PASSWORD
+const dbName = process.env.DB_NAME
+
+
 const uri =
-        `mongodb+srv://Theodorovieira01:5qbCv0iY7GDNvq5H@cluster0.8oerp.mongodb.net/DesafioDado?retryWrites=true&w=majority`
+        `mongodb+srv://Theodorovieira01:${dbPassword}@cluster0.8oerp.mongodb.net/${dbName}?retryWrites=true&w=majority`
 mongoose.connect(uri ,{ 
         useNewUrlParser: true, useUnifiedTopology: true,});
 
